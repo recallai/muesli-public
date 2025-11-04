fc03618477d9e230efe22fea16213e466b4d1508
nickatrecall <nick.amello@recall.ai>
2025-11-04 16:08:20 -0500

    Fix broken webpack dependency.

--- webpack.rules.js
+++ webpack.rules.js
@@ -6,16 +6,6 @@ module.exports = [
     test: /native_modules[/\\].+\.node$/,
     use: 'node-loader',
   },
-  {
-    test: /[/\\]node_modules[/\\].+\.(m?js|node)$/,
-    parser: { amd: false },
-    use: {
-      loader: '@vercel/webpack-asset-relocator-loader',
-      options: {
-        outputAssetBase: 'native_modules',
-      },
-    },
-  },
   // Put your webpack loader rules in this array.  This is where you would put
   // your ts-loader configuration for instance:
   /**
